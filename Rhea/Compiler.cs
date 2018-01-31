5dcd8eecb1e10d5d98e63cf14df7aa4273bbba4a
Kyle Kestell <kyle@kestell.org>
2018-01-31 08:13:33 -0600

    Tabs

--- Rhea/Compiler.cs
+++ Rhea/Compiler.cs
@@ -9,168 +9,168 @@ using Rhea.Ast.Nodes;
 
 namespace Rhea
 {
-    public class Compiler
-    {
-        readonly string standardLibrary = @"
-            struct string {}
-            extern fun string#dup(str : string) -> string
-            extern fun string#trim(str : string) -> string
-            extern fun string#length(str : string) -> int64
-        ";
+	public class Compiler
+	{
+		readonly string standardLibrary = @"
+			struct string {}
+			extern fun string#dup(str : string) -> string
+			extern fun string#trim(str : string) -> string
+			extern fun string#length(str : string) -> int64
+		";
 
-        public string Compile(string source)
-        {
-            var stdlib = CompileModule(standardLibrary);
+		public string Compile(string source)
+		{
+			var stdlib = CompileModule(standardLibrary);
 
-            var module = CompileModule(source, stdlib);
+			var module = CompileModule(source, stdlib);
 
-            CheckModule(module);
+			CheckModule(module);
 
-            return module.ToString();
-        }
+			return module.ToString();
+		}
 
-        Module CompileModule(string source, Module parent = null)
-        {
-            source = source.Replace("\r\n", "\n");
+		Module CompileModule(string source, Module parent = null)
+		{
+			source = source.Replace("\r\n", "\n");
 
-            var input = new AntlrInputStream(source);
+			var input = new AntlrInputStream(source);
 
-            var lexer = new RheaLexer(input);
-            var tokens = new CommonTokenStream(lexer);
+			var lexer = new RheaLexer(input);
+			var tokens = new CommonTokenStream(lexer);
 
-            var parser = new RheaParser(tokens);
-            var tree = parser.module();
+			var parser = new RheaParser(tokens);
+			var tree = parser.module();
 
-            return new ModuleBuilder(parent).Visit(tree);
-        }
+			return new ModuleBuilder(parent).Visit(tree);
+		}
 
-        void CheckModule(Module module)
-        {
+		void CheckModule(Module module)
+		{
             // Return statements
 
-            foreach (var f in module.Functions)
-            {
-                var returnStatements = FindStatements<Return>(f.Block).ToList();
+			foreach (var f in module.Functions)
+			{
+				var returnStatements = FindStatements<Return>(f.Block).ToList();
 
-                if (f.Type.Name == "void")
-                {
+				if (f.Type.Name == "void")
+				{
                     // Void functions must return void
 
-                    foreach (var s in returnStatements)
-                    {
-                        if (s.Expression != null)
-                            throw new Exception($"Function `{f.Name}` must return `void`, but it returns a `{s.Expression.InferredType.Name}`.\n\n{s.Source}");
-                    }
-                }
-                else
-                {
+					foreach (var s in returnStatements)
+					{
+						if (s.Expression != null)
+							throw new Exception($"Function `{f.Name}` must return `void`, but it returns a `{s.Expression.InferredType.Name}`.\n\n{s.Source}");
+					}
+				}
+				else
+				{
                     // Non-void functions must return something
 
-                    if (!returnStatements.Any())
-                        throw new Exception($"Function {f.Name} must return a {f.Type.Name}, but it returns nothing.");
+					if (!returnStatements.Any())
+						throw new Exception($"Function {f.Name} must return a {f.Type.Name}, but it returns nothing.");
 
                     // And it must be the appropriate type
 
-                    foreach (var s in returnStatements)
-                    {
-                        if (s.Expression.InferredType != f.Type)
-                            throw new Exception($"Function {f.Name} must return a {f.Type.Name}, not a {s.Expression.InferredType.Name}.");
-                    }
-                }
-            }
+					foreach (var s in returnStatements)
+					{
+						if (s.Expression.InferredType != f.Type)
+							throw new Exception($"Function {f.Name} must return a {f.Type.Name}, not a {s.Expression.InferredType.Name}.");
+					}
+				}
+			}
 
             // The expressions in if statements must evaluate to a boolean
 
-            foreach (var function in module.Functions)
-            {
-                foreach (var ifStatement in FindStatements<If>(function.Block))
-                {
-                    var typeName = ifStatement.Expression.InferredType.Name;
+			foreach (var function in module.Functions)
+			{
+				foreach (var ifStatement in FindStatements<If>(function.Block))
+				{
+					var typeName = ifStatement.Expression.InferredType.Name;
 
-                    if (typeName != "bool")
-                        throw new Exception($"Expression must evaluate to a bool, but evaluates to a {typeName}");
-                }
-            }
+					if (typeName != "bool")
+						throw new Exception($"Expression must evaluate to a bool, but evaluates to a {typeName}");
+				}
+			}
 
             // Prevent multiple declarations of variables with the same name in the same scope
 
-            foreach (var function in module.Functions)
-            {
-                foreach (var variableDeclaration in FindStatements<VariableDeclaration>(function.Block))
-                {
-                    var duplicateDeclarations = variableDeclaration
-                        .ParentBlock
-                        .Statements
-                        .OfType<VariableDeclaration>()
-                        .Where(d => d.Name == variableDeclaration.Name);
+			foreach (var function in module.Functions)
+			{
+				foreach (var variableDeclaration in FindStatements<VariableDeclaration>(function.Block))
+				{
+					var duplicateDeclarations = variableDeclaration
+						.ParentBlock
+						.Statements
+						.OfType<VariableDeclaration>()
+						.Where(d => d.Name == variableDeclaration.Name);
 
-                    if (duplicateDeclarations.Count() > 1)
-                        throw new Exception($"Multiple declaration of variable {variableDeclaration.Name}");
-                }
-            }
+					if (duplicateDeclarations.Count() > 1)
+						throw new Exception($"Multiple declaration of variable {variableDeclaration.Name}");
+				}
+			}
 
             // Types of left and right sides of assignment statements must match
 
-            foreach (var function in module.Functions)
-            {
-                foreach (var assignment in FindStatements<Assignment>(function.Block))
-                {
-                    var declaration = assignment.ParentBlock.FindDeclaration(assignment.VariableName);
+			foreach (var function in module.Functions)
+			{
+				foreach (var assignment in FindStatements<Assignment>(function.Block))
+				{
+					var declaration = assignment.ParentBlock.FindDeclaration(assignment.VariableName);
 
-                    if (declaration == null)
-                        throw new Exception($"Can't find declaration for {assignment.VariableName}");
+					if (declaration == null)
+						throw new Exception($"Can't find declaration for {assignment.VariableName}");
 
-                    if (declaration.Type != assignment.Expression.InferredType)
-                        throw new Exception($"Types of left ({declaration.Type.Name}) and right ({assignment.Expression.InferredType.Name}) sides of assignment statement must match");
-                }
-            }
+					if (declaration.Type != assignment.Expression.InferredType)
+						throw new Exception($"Types of left ({declaration.Type.Name}) and right ({assignment.Expression.InferredType.Name}) sides of assignment statement must match");
+				}
+			}
 
             // Types of left and right sides of member assignment statements must match
 
-            foreach (var function in module.Functions)
-            {
-                foreach (var assignment in FindStatements<MemberAssignment>(function.Block))
-                {
-                    var variableDeclaration = assignment.ParentBlock.FindDeclaration(assignment.VariableName);
+			foreach (var function in module.Functions)
+			{
+				foreach (var assignment in FindStatements<MemberAssignment>(function.Block))
+				{
+					var variableDeclaration = assignment.ParentBlock.FindDeclaration(assignment.VariableName);
 
-                    if (variableDeclaration == null)
-                        throw new Exception($"Can't find declaration for {assignment.VariableName}");
+					if (variableDeclaration == null)
+						throw new Exception($"Can't find declaration for {assignment.VariableName}");
 
-                    var structDeclaration = assignment.ParentBlock.FindStruct(variableDeclaration.Type.Name);
+					var structDeclaration = assignment.ParentBlock.FindStruct(variableDeclaration.Type.Name);
 
-                    if (structDeclaration == null)
-                        throw new Exception($"Can't find declaration for {variableDeclaration.Type.Name}");
+					if (structDeclaration == null)
+						throw new Exception($"Can't find declaration for {variableDeclaration.Type.Name}");
 
-                    var member = structDeclaration.Members.SingleOrDefault(m => m.Name == assignment.MemberName);
+					var member = structDeclaration.Members.SingleOrDefault(m => m.Name == assignment.MemberName);
 
-                    if (member == null)
-                        throw new Exception($"Struct {structDeclaration.Name} has no member named {assignment.MemberName}");
+					if (member == null)
+						throw new Exception($"Struct {structDeclaration.Name} has no member named {assignment.MemberName}");
 
-                    if (member.Type != assignment.Expression.InferredType)
-                        throw new Exception($"Types of left ({member.Type.Name}) and right ({assignment.Expression.InferredType.Name}) sides of member assignment statement must match");
-                }
-            }
-        }
+					if (member.Type != assignment.Expression.InferredType)
+						throw new Exception($"Types of left ({member.Type.Name}) and right ({assignment.Expression.InferredType.Name}) sides of member assignment statement must match");
+				}
+			}
+		}
 
-        static IEnumerable<T> FindStatements<T>(Block scope)
-        {
-            var childStatements = new List<T>();
+		static IEnumerable<T> FindStatements<T>(Block scope)
+		{
+			var childStatements = new List<T>();
 
-            var scopes = scope
-                .Statements
-                .OfType<IStatementWithBlock>();
+			var scopes = scope
+				.Statements
+				.OfType<IStatementWithBlock>();
 
-            foreach (var statement in scopes)
-                childStatements.AddRange(FindStatements<T>(statement.Block));
+			foreach (var statement in scopes)
+				childStatements.AddRange(FindStatements<T>(statement.Block));
 
-            var returnStatements = scope
-                .Statements
-                .OfType<T>()
-                .ToList();
+			var returnStatements = scope
+				.Statements
+				.OfType<T>()
+				.ToList();
 
-            returnStatements.AddRange(childStatements);
+			returnStatements.AddRange(childStatements);
 
-            return returnStatements;
-        }
-    }
+			return returnStatements;
+		}
+	}
 }
--- Rhea/Program.cs
+++ Rhea/Program.cs
@@ -4,26 +4,26 @@ using System.IO;
 
 namespace Rhea
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var sw = new Stopwatch();
-            sw.Start();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
 
-            var inputFilename = args[0];
-            var outputFilename = $"{Path.GetFileNameWithoutExtension(inputFilename)}.c";
+			var inputFilename = args[0];
+			var outputFilename = $"{Path.GetFileNameWithoutExtension(inputFilename)}.c";
 
-            var src = File.ReadAllText(inputFilename);
+			var src = File.ReadAllText(inputFilename);
 
-            var output = new Compiler().Compile(src);
+			var output = new Compiler().Compile(src);
 
-            File.WriteAllText(outputFilename, output);
+			File.WriteAllText(outputFilename, output);
 
-            Console.WriteLine(output);
+			Console.WriteLine(output);
 
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed);
-        }
-    }
+			sw.Stop();
+			Console.WriteLine(sw.Elapsed);
+		}
+	}
 }
